486fda22dcadd738e1d445b7c6fa80abc4ff149c
WhatsApp API for developers <care@whapi.cloud>
2024-02-09 19:27:34 +0700

    Add comments

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
     "numberColumn": "A",
     "recievedMessageColumn": "C",
     "messageColumn": "B",
-    "token": "C",
-    "webhookUrl": "",
+    "token": "", // channel token
+    "webhookUrl": "", // Webhook Link to your server. At ( {server link} ), when POST is requested, processing occurs
     "sendDelay": 20000, // delay between mailings (1 sec = 1000)
     "port": "8080", // server port
     "spreadSheetUrl": "" // url format: https://docs.google.com/spreadsheets/d/.../edit
