d84b5b804a3efc6609c6b6d90d8174ae551b9b16
Whapi-Cloud <care@whapi.cloud>
2024-04-01 17:46:13 +0700

    git fix

--- types/Reciever.ts
+++ types/Reciever.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export type Reciever = {
     to: string;
     row: number;
     body: string;
-=======
-export type Reciever = {
-    to: string;
-    row: number;
-    body: string;
->>>>>>> 8465c35f64ac03ec6455dbe8b0b23c2c4cacd02c
   };
